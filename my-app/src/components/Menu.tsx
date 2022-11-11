91387bd5a91644e52f08339500d4f628f4dfb402
Daniel PC <daniel.barczynski@onet.pl>
2022-11-12 00:58:56 +0100

    update

---
+++ my-app/src/components/Menu.tsx
@@ -0,0 +1,11 @@
+const Menu = () => {
+  return (
+    <div className="menu">
+        <div><a href="\src\index.tsx">Menu Item1</a></div>
+        <div><a href="\src\menu-item.tsx">Menu Item2</a></div>
+        <div><a href="\src\menu-item.tsx">Menu Item3</a></div>
+    </div>
+  );
+}
+
+export default Menu
